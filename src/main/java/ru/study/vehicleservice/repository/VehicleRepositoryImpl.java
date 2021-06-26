a35d75c1ba7a10fa7106e4477d488a9bb255861e
Dmitriy Rysev <moowtf@gmail.com>
2021-06-26 10:35:04 +0300

    Исарпвил тест на репозиторий

--- src/main/java/ru/study/vehicleservice/repository/VehicleRepositoryImpl.java
+++ src/main/java/ru/study/vehicleservice/repository/VehicleRepositoryImpl.java
@@ -2,16 +2,17 @@ package ru.study.vehicleservice.repository;
 
 import java.util.List;
 import org.jooq.DSLContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.study.vehicleservice.jooq.Tables;
 import ru.study.vehicleservice.jooq.tables.pojos.Vehicles;
 
 @Repository
 public class VehicleRepositoryImpl implements VehicleRepository {
-
-  @Autowired
   DSLContext context;
+
+  public VehicleRepositoryImpl(DSLContext context) {
+    this.context = context;
+  }
 
   @Override
   public List<Vehicles> findAllVehicles() {
--- src/test/java/ru/study/vehicleservice/repository/VehicleRepositoryImplTest.java
+++ src/test/java/ru/study/vehicleservice/repository/VehicleRepositoryImplTest.java
@@ -1,9 +1,9 @@
 package ru.study.vehicleservice.repository;
 
 import static org.jooq.SQLDialect.POSTGRES;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static ru.study.vehicleservice.jooq.tables.Vehicles.VEHICLES;
 
+import java.sql.SQLException;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -12,10 +12,9 @@ import org.jooq.Result;
 import org.jooq.impl.DSL;
 import org.jooq.tools.jdbc.MockConnection;
 import org.jooq.tools.jdbc.MockDataProvider;
+import org.jooq.tools.jdbc.MockExecuteContext;
 import org.jooq.tools.jdbc.MockResult;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.study.vehicleservice.jooq.Tables;
 import ru.study.vehicleservice.jooq.tables.pojos.Vehicles;
@@ -24,41 +23,38 @@ import ru.study.vehicleservice.jooq.tables.records.VehiclesRecord;
 @SpringBootTest
 class VehicleRepositoryImplTest {
 
-  UUID uuid = UUID.randomUUID();
-  OffsetDateTime offsetDateTime = OffsetDateTime.now();
-
-  final MockDataProvider myMockProvider = context -> {
-    final DSLContext context1 = DSL.using(POSTGRES);
-    final Result<VehiclesRecord> resultRecord = context1.newResult(Tables.VEHICLES);
-
-    resultRecord.add(
-        context1.newRecord(Tables.VEHICLES).value1(1L).value2("C143FL").value3("166").value4(uuid)
-            .value5(3).value6(108).value7(1).value8(true).value9(offsetDateTime));
-
-    return new MockResult[]{new MockResult(1, resultRecord)};
-  };
-
-  final DSLContext mockedDsl = DSL.using(new MockConnection(myMockProvider), POSTGRES);
-
-  @InjectMocks
-  VehicleRepositoryImpl vehicleRepository;
-
-  @Mock
-  DSLContext context;
-
   @Test
-  void findAllVehicles() {
+  void shouldFindAllVehicles() {
+    MockConnection connection = new MockConnection(new AllVehiclesMockDataProvider());
+    DSLContext context = DSL.using(connection, POSTGRES);
+    List<Vehicles> vehicles = new VehicleRepositoryImpl(context).findAllVehicles();
+  }
 
-    when(context.selectFrom(Tables.VEHICLES))
-        .thenReturn(mockedDsl.selectFrom(Tables.VEHICLES));
+  static class AllVehiclesMockDataProvider implements MockDataProvider {
 
-    List<Vehicles> mockVehicles = mockedDsl.selectFrom(Tables.VEHICLES).fetchInto(Vehicles.class);
+    @Override
+    public MockResult[] execute(MockExecuteContext ctx) throws SQLException {
+      UUID uuid = UUID.randomUUID();
+      OffsetDateTime offsetDateTime = OffsetDateTime.now();
 
-    List<Vehicles> resultVehicles = vehicleRepository.findAllVehicles();
+      DSLContext context = DSL.using(POSTGRES);
 
-    assertEquals(mockedDsl.selectFrom(Tables.VEHICLES).fetchInto(Vehicles.class),
-        vehicleRepository.findAllVehicles());
+      Result<VehiclesRecord> recordResult = context.newResult(VEHICLES);
 
+      recordResult.add(
+          context
+              .newRecord(Tables.VEHICLES)
+              .value1(1L)
+              .value2("C143FL")
+              .value3("166")
+              .value4(uuid)
+              .value5(3)
+              .value6(108)
+              .value7(1)
+              .value8(true)
+              .value9(offsetDateTime));
 
+      return new MockResult[] {(new MockResult(1, recordResult))};
+    }
   }
 }
