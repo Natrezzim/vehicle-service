cdc8abc3c13b6840df5054736bc02a2df0c50b57
Dmitriy Rysev <moowtf@gmail.com>
2021-06-29 16:15:13 +0300

    Исарпвил тест на репозиторий

--- src/test/java/ru/study/vehicleservice/repository/VehicleRepositoryImplTest.java
+++ src/test/java/ru/study/vehicleservice/repository/VehicleRepositoryImplTest.java
@@ -1,9 +1,9 @@
 package ru.study.vehicleservice.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.jooq.SQLDialect.POSTGRES;
 import static ru.study.vehicleservice.jooq.tables.Vehicles.VEHICLES;
 
-import java.sql.SQLException;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -11,50 +11,47 @@ import org.jooq.DSLContext;
 import org.jooq.Result;
 import org.jooq.impl.DSL;
 import org.jooq.tools.jdbc.MockConnection;
-import org.jooq.tools.jdbc.MockDataProvider;
-import org.jooq.tools.jdbc.MockExecuteContext;
 import org.jooq.tools.jdbc.MockResult;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.study.vehicleservice.jooq.Tables;
 import ru.study.vehicleservice.jooq.tables.pojos.Vehicles;
 import ru.study.vehicleservice.jooq.tables.records.VehiclesRecord;
 
-@SpringBootTest
 class VehicleRepositoryImplTest {
 
   @Test
   void shouldFindAllVehicles() {
-    MockConnection connection = new MockConnection(new AllVehiclesMockDataProvider());
+
+    MockConnection connection =
+        new MockConnection(
+            ctx -> {
+              DSLContext context = DSL.using(POSTGRES);
+              Result<VehiclesRecord> recordResult = context.newResult(VEHICLES);
+              recordResult.add(
+                  context
+                      .newRecord(Tables.VEHICLES)
+                      .value1(1L)
+                      .value2("C143FL")
+                      .value3("166")
+                      .value4(UUID.randomUUID())
+                      .value5(3)
+                      .value6(108)
+                      .value7(1)
+                      .value8(true)
+                      .value9(OffsetDateTime.now()));
+              return new MockResult[] {(new MockResult(1, recordResult))};
+            });
+
+    UUID uuid = UUID.randomUUID();
+    OffsetDateTime offsetDateTime = OffsetDateTime.now();
+
     DSLContext context = DSL.using(connection, POSTGRES);
     List<Vehicles> vehicles = new VehicleRepositoryImpl(context).findAllVehicles();
-  }
+    List<Vehicles> expectedVehicles =
+        List.of(new Vehicles(1L, "C143FL", "166", uuid, 3, 108, 1, true, offsetDateTime));
 
-  static class AllVehiclesMockDataProvider implements MockDataProvider {
-
-    @Override
-    public MockResult[] execute(MockExecuteContext ctx) throws SQLException {
-      UUID uuid = UUID.randomUUID();
-      OffsetDateTime offsetDateTime = OffsetDateTime.now();
-
-      DSLContext context = DSL.using(POSTGRES);
-
-      Result<VehiclesRecord> recordResult = context.newResult(VEHICLES);
-
-      recordResult.add(
-          context
-              .newRecord(Tables.VEHICLES)
-              .value1(1L)
-              .value2("C143FL")
-              .value3("166")
-              .value4(uuid)
-              .value5(3)
-              .value6(108)
-              .value7(1)
-              .value8(true)
-              .value9(offsetDateTime));
-
-      return new MockResult[] {(new MockResult(1, recordResult))};
-    }
+    assertThat(vehicles)
+        .usingElementComparatorIgnoringFields("vehicleUuid", "createdAt")
+        .isEqualTo(expectedVehicles);
   }
 }
